dcc93b394912356e2ed389720de044b9f5f74bc3
Goun Lee <goun.lee@lge.com>
2019-09-26 15:09:59 +0900

    Update license

--- test/testJSONResourceFile.js
+++ test/testJSONResourceFile.js
@@ -1,7 +1,7 @@
 /*
  * testJSONResourceFile.js - test the JavaScript file handler object.
  *
- * Copyright © 2019, Box, Inc.
+ * Copyright © 2019, JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
